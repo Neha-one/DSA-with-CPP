b7d393fbfcf010eecb9de3feabd587787dcaadb6
Neha-one <justforgtb@gmail.com>
2026-01-12 18:20:20 +0530

    update these files

--- 01_Basics/01_Input_Output/sum.cpp
+++ 01_Basics/01_Input_Output/sum.cpp
@@ -1,30 +1,20 @@
 // EXAMPLE:calculating sum of two numbers.
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int x,y;
-//     cout << "enter your 1st number: ";
-//     cin >> x;
-//     cout << "enter your  2nd number: ";
-//     cin >> y;
-//     int sum = x + y;
-//     cout << "sum of number is: " << sum;
-    
-// }
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, y;
+    cout << "enter your 1st number: ";
+    cin >> x;
+    cout << "enter your  2nd number: ";
+    cin >> y;
+    int sum = x + y;
+    cout << "sum of number is: " << sum;
 
-// EXAMPLE:calculating sum of three numbers.
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     int x,y,z;
-//     cout<<"enter 1st number: ";
-//     cin>>x;
-//     cout<<"enter 2nd number: ";
-//     cin>>y;
-//     cout<<"enter 3rd number: ";
-//     cin>>z;
-//     int sum=x+y+z;
-//     cout<<"sum of these numbers is: ";
-//     cout<<sum;
-// }
+    cout<<endl;
+    // sum of n numbers using formula:
+    int n;
+    cout << "enter number:";
+    cin >> n;
+    cout << "sum of " << n << " number is " << (n*(n + 1)) / 2;
+}

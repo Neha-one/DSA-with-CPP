9e7b23bc780aeb104736985bc89a5b4ac1c63cca
Neha-one <justforgtb@gmail.com>
2026-03-17 07:25:04 +0530

    update these files

--- 01_Basics/02_Conditionals/NumberOfNOte.cpp
+++ 01_Basics/02_Conditionals/NumberOfNOte.cpp
@@ -1,29 +1,33 @@
-// count number of notes of 100rs , 50rs, 20rs and 1 in the given amount 1330.
+// count number of notes of 100rs , 50rs, 20rs,10rs and 1 in the given amount 1330.
 #include <iostream>
 using namespace std;
 
 int main()
 {
 
-    int amount = 1330;
-    int note;
-    cout << "enter any 100 , 50, 20, 1rs note:";
-    cin >> note;
-    switch (note)
+    int amount;
+    cout << "enter amount";
+    cin >> amount;
+
+    switch (1)
     {
-    case 100:
-        cout << "total number of 100rs note in the amount " << amount << " is = " << amount / 100;
-        break;
-    case 50:
-        cout << "total number of 50rs note in the amount " << amount << " is = " << amount / 50;
-        break;
-    case 20:
-        cout << "total number of 20rs note in the amount " << amount << " is = " << amount / 20;
-        break;
     case 1:
-        cout << "total number of 1rs note in the amount " << amount << " is = " << amount / 1;
-        break;
-    default:
-        cout << "invalid note";
+        cout << "100rs note is = " << amount / 100 << endl;
+        amount = amount % 100;
+
+    case 2:
+        cout << "50rs note is = " << amount / 50 << endl;
+        amount = amount % 50;
+
+    case 3:
+        cout << "20rs note is = " << amount / 20 << endl;
+        amount = amount % 20;
+
+    case 4:
+        cout << "1rs note is = " << amount / 10 << endl;
+        amount = amount % 10;
+    case 5:
+        cout << "1rs note is = " << amount;
     }
+    return 0;
 }
---
+++ 01_Basics/02_Conditionals/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+50, 20, 1rs note:";
+    // cin >> note;

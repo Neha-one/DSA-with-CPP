e0afc9ad6c700708cd7629dc976423f015ce91cd
Neha-one <justforgtb@gmail.com>
2026-05-25 23:10:50 +0530

    add these files

---
+++ Array/MaxMinOfArr.cpp
@@ -0,0 +1,22 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+
+//   int Narr[] = {23, 4, 2, 56, 3, 9};
+//   int NarrSize = sizeof(Narr) / sizeof(int);
+//   int i = 0;
+//   while (i < NarrSize)
+//   {
+//     if (Narr[i] < Narr[i + 1])
+//     {
+//       i = i + 1;
+//     }
+//     else
+//     {
+//       i = i;
+//     }
+//   };
+//   cout << Narr[i];
+//   return 0;
+// };

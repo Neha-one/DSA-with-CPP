c784de72eef975b4b26386e002cd5ca524d30e01
Neha-one <justforgtb@gmail.com>
2026-06-16 15:55:52 +0530

    add these files

--- 02-Array/swapAlternate.cpp
+++ 02-Array/swapAlternate.cpp
@@ -1,8 +1,56 @@
 #include <iostream>
 using namespace std;
 
+void swapalternate(int arr[], int n)
+{
+    // it not good as a dsa practice because the array remains unchanged and we just change the order of elements and print that.
+    for (int i = 1; i < n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            i++;
+        }
+        cout << arr[i] << " ";
+
+        for (int j = 0; j <= i; j++)
+        {
+            if (n % 2 != 0 && i / 2 == n / 2)
+            {
+                break;
+            }
+            if (i - j == 1)
+            {
+                cout << arr[j] << " ";
+            }
+        }
+    }
+    cout << endl;
+}
+void swapusingfn(int arr[], int n)
+{
+    for (int i = 0; i < n; i += 2)
+    {
+        if (i + 1 < n)
+        {
+
+            swap(arr[i], arr[i + 1]);
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
 int main()
 {
-    
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    int n = sizeof(arr) / sizeof(int);
+
+    //---------method -01----------(my 1st attempt idea😊)
+
+    swapalternate(arr, n);
+
+    //---------method - 02----------
+    swapusingfn(arr, n);
     return 0;
 }
--- 02-Array/uniqueInArray.cpp
+++ 02-Array/uniqueInArray.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 void findUniqueEl(int arr[], int n)
 {
-  int temp = arr[0];
-  int count = 1;
+
   for (int i = 0; i < n; i++)
   {
     int count = 0;
@@ -18,6 +17,7 @@ void findUniqueEl(int arr[], int n)
     if (count == 1)
       cout << arr[i] << " ";
   }
+  cout << endl;
 }
 
 int main()
@@ -26,4 +26,15 @@ int main()
   int n = sizeof(arr) / sizeof(int);
 
   findUniqueEl(arr, n);
+
+  //----------method 02 ------------------
+  // it works only when only one number is once time and onother is twice times.
+  int arr1[] = {6, 6, 4, 11, 11, 3, 3, 5, 5};
+
+  int ans = 0;
+  for (int i = 0; i < n; i++)
+  {
+    ans = ans ^ arr1[i];
+  }
+  cout << ans;
 }

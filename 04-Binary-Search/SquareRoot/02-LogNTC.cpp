5c98b14f3cf4e54022048b2eb8e2a176f7f9f107
Neha-one <justforgtb@gmail.com>
2026-07-11 21:50:51 +0530

    add these files

--- 04-Binary-Search/SquareRoot/02-LogNTC.cpp
+++ 04-Binary-Search/SquareRoot/02-LogNTC.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int squareRoot()
+int squareRoot(int key)
 {
 
-  int key = 18;
   int s = 0;
   int e = key;
-  long long int mid = s + (e - s) / 2;
-  long long int ans = -1;
-  while (s <=  e)
+  int mid = s + (e - s) / 2;
+  int ans = -1;
+
+  while (s <= e)
   {
 
     long long int square = mid * mid;
@@ -30,7 +30,38 @@ int squareRoot()
   }
   return ans;
 }
+double pointVal(float num, float key)
+{
+  double fact = 0;
+  double answer = 0;
+  for (float i = 1; i <= 9; i++)
+  {
+    fact = num + i / 10;
+    double squire = fact * fact;
+    if (squire == key)
+    {
+
+      return fact;
+    }
+    for (float j = 0; j <= 9; j++)
+    {
+      answer = fact + j / 100;
+      // cout << "hey its fact: " << fact << endl;
+      double squreAnswer = answer * answer;
+      // cout << "hey its answer: " << squreAnswer << endl;
+      if (squreAnswer == key)
+      {
+        return answer;
+      }
+    }
+  }
+  return 0;
+}
 int main()
 {
-  cout << squareRoot();
+  float key = 0.09;
+  int num = squareRoot(key);
+  cout << num << endl;
+  float ans = pointVal(num, key);
+  cout << ans;
 }

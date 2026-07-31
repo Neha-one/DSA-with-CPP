36ab2d2dda7d478b4122bfce8a7886f9cd1fee6a
Neha-one <justforgtb@gmail.com>
2026-07-31 17:28:56 +0530

    add this file

---
+++ 05-Sorting/selection-sort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int selectionSort(int arr[], int n)
+{
+  for (int i = 0; i < n - 1; i++)
+  {
+    int minIndex = i;
+    for (int j = i + 1; j < n; j++)
+    {
+      if (arr[minIndex] > arr[j])
+      {
+        minIndex = j;
+      }
+    }
+    swap(arr[minIndex], arr[i]);
+  }
+  // return arr[];
+}
+int main()
+{
+  int arr[] = {3, 2, 46, 1, 8};
+  int n = sizeof(arr) / sizeof(int);
+
+  selectionSort(arr, n);
+
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << " ";
+  }
+  return 0;
+}

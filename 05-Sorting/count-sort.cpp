b7a96f187f013ff0592e70543e55e97c04286abd
Neha-one <justforgtb@gmail.com>
2026-09-13 15:36:06 +0530

    update this dsa problem

--- 05-Sorting/count-sort.cpp
+++ 05-Sorting/count-sort.cpp
@@ -1,28 +1,41 @@
 #include <iostream>
 using namespace std;
+void printArr(int arr[], int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << " ";
+  }
+}
 void countSort(int arr[], int n)
 {
   int maxVal = INT16_MIN;
-  int minVal = INT16_MAX;
   for (int i = 0; i < n; i++)
   {
     maxVal = max(maxVal, arr[i]);
-    minVal = min(minVal, arr[i]);
   }
-  int freq[maxVal] = {0};
+  int freq[maxVal + 1] = {0};
   for (int i = 0; i < n; i++)
   {
     int idx = arr[i];
     freq[idx]++;
   }
+  int j = 0;
   for (int i = 0; i <= maxVal; i++)
   {
-    while ()
+    while (freq[i] != 0)
+    {
+      arr[j] = i;
+      freq[i]--;
+      j++;
+    }
   }
+  cout << "counting sort: ";
+  printArr(arr, n);
 }
 int main()
 {
-  int arr[] = {1, 4, 1, 3, 2, 4, 3, 7};
+  int arr[] = {1, 4, 1, 3, 2, 4, 3, 0, 0, 0, 7};
   int n = sizeof(arr) / sizeof(int);
   countSort(arr, n);
   return 0;
